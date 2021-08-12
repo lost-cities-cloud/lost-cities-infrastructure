b9733c35b76775646b34211e9279b387781d8ee6
Derek <contact@dereknelson.io>
2021-08-12 16:54:33 -0400

    Updating init script

--- init.sh
+++ init.sh
@@ -4,7 +4,11 @@ WORKSPACE="${HOME}/Workspace"
 
 INFRASTRUCTURE_REPO="git@github.com:lostcities-cloud/lostcities-infrastructure.git"
 COMMON_REPO="git@github.com:lostcities-cloud/lostcities-common.git"
+ACCOUNTS_REPO="git@github.com:lostcities-cloud/lostcities-accounts.git"
+MATCHES_REPO="git@github.com:lostcities-cloud/lostcities-matches.git"
 EXPERIENCE_REPO="git@github.com:lostcities-cloud/lostcities-web-experience.git"
+
+
 
 mkdir -p "$WORKSPACE"
 
@@ -12,5 +16,6 @@ cd "$WORKSPACE" || exit
 
 git clone $INFRASTRUCTURE_REPO
 git clone $EXPERIENCE_REPO
+git clone $ACCOUNTS_REPO
+git clone $MATCHES_REPO
 git clone $COMMON_REPO
-
