3e218ec3bf998eecaaba69c22cdd1d4854906b3a
Derek <contact@dereknelson.io>
2022-02-27 00:45:39 -0500

    Adding Gamestate and player events

--- bin/build-images.sh
+++ bin/build-images.sh
@@ -7,7 +7,7 @@ cd "$WORKSPACE_ROOT" || exit
 function gradleBuild() {
   echo "Building $1"
   cd "$1" || exit
-  time ./gradlew bootBuildImage 1> /dev/null &
+  time ./gradlew clean bootBuildImage 1> /dev/null &
 }
 
 for project_dir in $GRADLE_PROJECTS; do
